67b63112e281c7bc985af7da33f43efe2c163c1a
MadsStaalMedia <msan71643@edu.ucl.dk>
2025-12-03 11:19:42 +0100

    arbejde på scroll knapper

--- js/opholdScroll.js
+++ js/opholdScroll.js
@@ -1,7 +1,20 @@
 const tentIcons = document.getElementsByClassName("tentIcon");
 
-const tentButtons = [
-{ href="ophold.html#silva" title="Vis Silva →" },
-{ href="ophold.html#aqua" title="Vis Aqua →" },
-{ href="ophold.html#terra" title="Vis Terra →"}
+const tentButtons = document.getElementById("tentButtons");
+
+const tentScroll = [
+{ href:"ophold.html#silva", title:"Vis Silva →", id:"silvaScroll" },
+{ href:"ophold.html#aqua", title:"Vis Aqua →", id:"aquaScroll" },
+{ href:"ophold.html#terra", title:"Vis Terra →", id:"terraScroll" }
 ];
+
+for (i = 0; i < tentScroll.length; i++) {
+
+    tentButtons.innerHTML += '<div id="'+tentScroll[i].id+'">';
+
+    let scrollArea = document.getElementById(tentScroll[i].id);
+
+    scrollArea.innerHTML += tentIcons[i].innerHTML;
+    scrollArea.innerHTML += '<p><a href='+tentScroll[i].href+'>'+tentScroll[i].title+'</a></p>'
+
+}
