17b74fa14f68083704bbad9aaccfc2e9ea6857d4
MadsStaalMedia <msan71643@edu.ucl.dk>
2025-12-03 11:01:58 +0100

    Update opholdScroll.js

--- js/opholdScroll.js
+++ js/opholdScroll.js
@@ -1,5 +1,7 @@
 const tentIcons = document.getElementsByClassName("tentIcon");
 
 const tentButtons = [
-{  }
+{ href="ophold.html#silva" title="Vis Silva →" },
+{ href="ophold.html#aqua" title="Vis Aqua →" },
+{ href="ophold.html#terra" title="Vis Terra →"}
 ];
