31eee2a10a73b7bbcf8a066d821a8c8e5e04f542
MadsStaalMedia <msan71643@edu.ucl.dk>
2025-12-03 09:41:04 +0100

    hover

--- js/bookButton.js
+++
@@ -1,7 +0,0 @@
-const bookButton = document.getElementsByClassName("book");
-
-for (i = 0; i < bookButton.length ; i++) {
-    bookButton[i].addEventListener("click", function(){
-        window.location = "booking.html";
-    });
-}
