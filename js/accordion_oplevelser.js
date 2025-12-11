f198e1552a4784304700415ad385768915f1b4ba
svendsteen <svendkj2@hotmail.com>
2025-12-11 19:41:40 +0100

    Tilføjelse af kilde (Svend)

--- js/accordion_oplevelser.js
+++ js/accordion_oplevelser.js
@@ -14,3 +14,6 @@ for (let i=0; i < acc.length; i++) {
         }
     });
 }
+
+//Kilder & inspiration
+//https://www.w3schools.com/howto/howto_js_accordion.asp - How to create an accordion (hentet d. 11 december 2025)
